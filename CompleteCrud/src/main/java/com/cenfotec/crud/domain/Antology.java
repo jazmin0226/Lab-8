ae7db396425aaf71c87084e75c3bf3dd049e0c19
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-11-16 00:22:03 -0600

    Initial commit

---
+++ CompleteCrud/src/main/java/com/cenfotec/crud/domain/Antology.java
@@ -0,0 +1,74 @@
+package com.cenfotec.crud.domain;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Antology {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private String subtitulo;
+
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date publication;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="anthology")
+	private Set<Article> articles;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(Set<Article> articles) {
+		this.articles = articles;
+	}
+
+	public String getSubtitulo() {
+		return subtitulo;
+	}
+
+	public void setSubtitulo(String subtitulo) {
+		this.subtitulo = subtitulo;
+	}
+
+	public Date getPublication() {
+		return publication;
+	}
+
+	public void setPublication(Date publication) {
+		this.publication = publication;
+	}
+}
