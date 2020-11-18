1dcb4980659748e7083ccd92ad521a921f71945b
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-11-17 19:19:40 -0600

    actualizacion

--- CompleteCrud/src/main/java/com/cenfotec/crud/repo/AntologyRepository.java
+++ CompleteCrud/src/main/java/com/cenfotec/crud/repo/AntologyRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cenfotec.crud.domain.Antology;
 
 public interface AntologyRepository extends JpaRepository<Antology, Long>{
-	public List<Antology> findByNameContaining(String word);
+	public List<Antology> findByNameContaining(String name);
 }
